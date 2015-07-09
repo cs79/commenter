0ae02eaf3c6084fba15d7bce9b8f05c8e447508d
Alexander Lee <cloudstrife79@gmail.com>
2015-07-09 18:15:47 -0400

    initial commit with basic comment append functionality

---
+++ app.js
@@ -0,0 +1,9 @@
+var main = function () {
+	"use strict";
+
+	$('.comment-input button').on('click', function(event) {
+		$('.right-output').append('<p>This is a new comment!</p>');
+	});
+};
+
+$(document).ready(main);
