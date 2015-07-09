47b022fe8b4285eb757ed8733b612b4863721043
Alexander Lee <cloudstrife79@gmail.com>
2015-07-09 18:54:51 -0400

    add fadein comments on enter or button click

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var main = function () {
 	"use strict";
 
-	$('.comment-input button').on('click', function(event) {
-		$('.right-output').append('<p>This is a new comment!</p>');
+	var addCommentFromInputBox = function () {
+		var $new_comment;
+
+		if ($('.comment-input input').val() !== "") {
+			$new_comment = $('</p>').text($('.comment-input input').val());
+			$new_comment.hide();
+			$('.right-output').append($new_comment);
+			$new_comment.fadeIn();
+			$('.comment-input input').val("");	
+		}
+	};
+
+
+	$('.comment-input button').on('click', function (event) {
+		addCommentFromInputBox();
+		
+	});
+
+	$('.comment-input input').on('keypress', function (event) {
+		if (event.keyCode == '13') {
+			addCommentFromInputBox();
+		}
 	});
 };
 
